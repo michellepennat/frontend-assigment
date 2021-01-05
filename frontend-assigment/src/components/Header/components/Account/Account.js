839261b46e017248dcf13cd324f6f7dc88a38fa5
michellepennat <mp.design.apps@gmail.com>
2021-01-05 10:03:09 -0500

    refactor(Account): add style dropdown profile

--- frontend-assigment/src/components/Header/components/Account/Account.js
+++ frontend-assigment/src/components/Header/components/Account/Account.js
@@ -1,18 +1,43 @@
 import React from "react";
+import { Menu, Dropdown } from "antd";
+
+import DownSvg from "../../../../assets/icons/down.svg";
+import ProfileJpg from "../../../../assets/images/profile.jpg";
+
+const menu = (
+  <Menu>
+    <Menu.Item key="0">
+      <a href="/account">Mi perfil</a>
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="1">
+      <a href="/notification">Notificaciones</a>
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="2">
+      <a href="/configuration">Configuración</a>
+    </Menu.Item>
+  </Menu>
+);
 
 const Account = () => {
   return (
-    <div className="header__account">
-      <div className="header__account__notification">
+    <div className="account">
+      <div className="account__notification">
         <p>1</p>
         <img alt="Notificacion" />
       </div>
-      <div className="header__account__profile">
-        <img alt="Foto perfil" />
-        <img alt="Desplegar menu" />
-        <div style={{ display: "none" }}>lo que esta en el drop</div>
-      </div>
-      <div className="header__account__settings">
+      <Dropdown placement="bottomRight" className="account__profile" overlay={menu} trigger={["click"]}>
+        <button
+          className="account__profile__button"
+          onClick={(e) => e.preventDefault()}
+        >
+          <img className="photo" src={ProfileJpg} alt="Foto perfil" />
+
+          <img src={DownSvg} alt="Desplegar menu" className="down" />
+        </button>
+      </Dropdown>
+      <div className="account__settings">
         <a href="/settings">
           <img alt="Ajustes" />
         </a>
