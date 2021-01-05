ea03d46c0e3b502b92cd97445dd2f5cbe1198d92
michellepennat <mp.design.apps@gmail.com>
2021-01-05 12:17:36 -0500

    refactor(Footer): add styles

--- frontend-assigment/src/components/Footer/Footer.js
+++ frontend-assigment/src/components/Footer/Footer.js
@@ -1,10 +1,18 @@
 import React from "react";
 
+import LogoPng from "../../assets/images/logo.png";
+
 const Footer = (props) => {
   return (
-    <div className="footer">
-      Im footer
-    </div>
+    <footer>
+      <img className="logo" src={LogoPng} alt="Logo" />
+      <div className="information"> 
+        <a href="/questions">Preguntas frecuentes</a>
+        <a href="/contactus">Contáctanos</a>
+        <a href="/information">Información</a>
+      </div>
+      <span>Docred © 2019. Todos los derechos reservados</span>
+    </footer>
   );
 };
 
