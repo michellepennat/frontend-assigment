d4c7b77b063e464103bf40c421c8cfe30d610947
michellepennat <mp.design.apps@gmail.com>
2021-01-05 12:19:50 -0500

    refactor(Related): add content

--- frontend-assigment/src/components/Related/Related.js
+++ frontend-assigment/src/components/Related/Related.js
@@ -2,8 +2,32 @@ import React from "react";
 
 const Related = () => {
   return (
-    <div className="related" style={{ width:'10%' }}>
-      Im related
+    <div className="related">
+      <h6>Noticias relacionadas</h6>
+      <div>
+        <div>
+          <img alt="Imagen de referencia" />
+          <div>
+            <h5>Title</h5>
+            <p>Autor</p>
+            <hr />
+            <div>
+              <button>
+                <img alt="Destacar" />
+                <span>50</span>
+              </button>
+              <button>
+                <img alt="Comentar" />
+                <span>40</span>
+              </button>
+              <button>
+                <img alt="Visualizaciones" />
+                <span>75</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
