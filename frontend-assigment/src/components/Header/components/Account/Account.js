48e28197702c918e856d43f02b4a78e5dcff5874
michellepennat <mp.design.apps@gmail.com>
2021-01-05 07:47:24 -0500

    refactor(Account): add file style sass

--- frontend-assigment/src/components/Header/components/Account/Account.js
+++ frontend-assigment/src/components/Header/components/Account/Account.js
@@ -2,17 +2,17 @@ import React from "react";
 
 const Account = () => {
   return (
-    <div>
-      <div>
+    <div className="header__account">
+      <div className="header__account__notification">
         <p>1</p>
         <img alt="Notificacion" />
       </div>
-      <div>
+      <div className="header__account__profile">
         <img alt="Foto perfil" />
         <img alt="Desplegar menu" />
         <div style={{ display: "none" }}>lo que esta en el drop</div>
       </div>
-      <div>
+      <div className="header__account__settings">
         <a href="/settings">
           <img alt="Ajustes" />
         </a>
