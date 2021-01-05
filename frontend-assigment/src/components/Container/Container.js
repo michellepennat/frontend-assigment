6610338cba91f608b44964eebd6383c7e38f9069
michellepennat <mp.design.apps@gmail.com>
2021-01-05 12:18:19 -0500

    refactor(News): add content

--- frontend-assigment/src/components/Container/Container.js
+++ frontend-assigment/src/components/Container/Container.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Container = (props) => {
   return (
-    <div className="container" style={{ width: "100%", display:'flex' }}>
+    <main>
       {props.children}
-    </div>
+    </main>
   );
 };
 
--- frontend-assigment/src/components/News/News.js
+++ frontend-assigment/src/components/News/News.js
@@ -2,9 +2,38 @@ import React from "react";
 
 const News = (props) => {
   return (
-    <div className="news" style={{ width: "90%" }}>
+    <section className="news">
       <img alt="Banner noticia" />
-    </div>
+      <h1>Title</h1>
+      <button>
+        <p>Guardar</p>
+      </button>
+      <div>
+        <img alt="Imagen de la noticia"/>
+        <h5>Noticiar DocRed</h5>
+        <div>
+          <span>Hace 12 h | Publicado: 17/01/18 | Lectura: 3 min</span>
+        </div>
+      </div>
+      <div>
+        <div>Medicina Interna</div>
+      </div>
+      <p>Lorem ipsum</p>
+      <div>
+        <button>
+          <img alt="Destacar" />
+          <span>Destacar</span>
+        </button>
+        <button>
+          <img alt="Comentar" />
+          <span>Comentar</span>
+        </button>
+        <button>
+          <img alt="Compartir" />
+          <span>Compartir</span>
+        </button>
+      </div>
+    </section>
   );
 };
 
