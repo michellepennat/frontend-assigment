9f2ec1dc4b0cb5da68325fa6e1df531dab166133
michellepennat <mp.design.apps@gmail.com>
2021-01-05 20:01:39 -0500

    refactor(Related): add style for web

--- src/components/Related/Related.js
+++ src/components/Related/Related.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-import ImageNewsJpg from "../../assets/images/image-new.jpg";
 import StarSvg from "../../assets/icons/star.svg";
 import CommentsSvg from "../../assets/icons/comments.svg";
 import EyeSvg from "../../assets/icons/ver.svg";
+import { relatedDummy } from "./relatedDummy";
 
 const Related = () => {
   return (
@@ -13,38 +13,49 @@ const Related = () => {
         <hr />
       </div>
       <div className="related__items">
-        <div className="related__items__item">
-          <div style={{ position: "relative" }}>
-            <img
-              src={ImageNewsJpg}
-              alt="Imagen de referencia"
-              className="image-item"
-            />
-            <div className="related__items__item__tag">
-              <p>NOTICIA</p>
+        {relatedDummy.map((relatedItem, i) => (
+          <div key={i}>
+            <div
+              className="related__items__item"
+              style={
+                i === 0
+                  ? { marginTop: 0, marginBottom: 10 }
+                  : { margin: "10px 0" }
+              }
+            >
+              <div style={{ position: "relative" }}>
+                <img
+                  src={relatedItem.image}
+                  alt="Imagen de referencia"
+                  className="image-item"
+                />
+                <div className="related__items__item__tag">
+                  <p>NOTICIA</p>
+                </div>
+              </div>
+              <div className="related__items__item__information">
+                <h5>{relatedItem.title}</h5>
+                <span>Por: {relatedItem.autor} </span>
+                <hr />
+                <div className="buttons">
+                  <button>
+                    <img src={StarSvg} alt="Destacar" />
+                    <span>50</span>
+                  </button>
+                  <button>
+                    <img src={CommentsSvg} alt="Comentar" />
+                    <span>40</span>
+                  </button>
+                  <button>
+                    <img src={EyeSvg} alt="Visualizaciones" />
+                    <span>75</span>
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="related__items__item__information">
-            <h5>Title</h5>
-            <span>Autor</span>
             <hr />
-            <div className="buttons">
-              <button>
-                <img src={StarSvg} alt="Destacar" />
-                <span>50</span>
-              </button>
-              <button>
-                <img src={CommentsSvg} alt="Comentar" />
-                <span>40</span>
-              </button>
-              <button>
-                <img src={EyeSvg} alt="Visualizaciones" />
-                <span>75</span>
-              </button>
-            </div>
           </div>
-        </div>
-        <hr />
+        ))}
       </div>
     </section>
   );
---
+++ src/components/Related/relatedDummy.js
@@ -0,0 +1,29 @@
+import Image from "../../assets/images/image-new.jpg";
+
+export const relatedDummy = [
+  {
+    image: Image,
+    title: "Lorem ipsum dolor: Voltpat consequat",
+    autor: "Lorem Ipsum Dolor",
+  },
+  {
+    image: Image,
+    title: "Lorem ipsum dolor: Voltpat consequat",
+    autor: "Lorem Ipsum Dolor",
+  },
+  {
+    image: Image,
+    title: "Lorem ipsum dolor: Voltpat consequat",
+    autor: "Lorem Ipsum Dolor",
+  },
+  {
+    image: Image,
+    title: "Lorem ipsum dolor: Voltpat consequat",
+    autor: "Lorem Ipsum Dolor",
+  },
+  {
+    image: Image,
+    title: "Lorem ipsum dolor: Voltpat consequat",
+    autor: "Lorem Ipsum Dolor",
+  },
+];
